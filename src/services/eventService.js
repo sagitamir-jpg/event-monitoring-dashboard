f4968bebf2ffaf99f490833854b1da128feffdb5
sagitamir-jpg <sagi.tamir@gmail.com>
2026-01-03 12:00:43 -0500

    Create eventService.js

---
+++ src/services/eventService.js
@@ -0,0 +1,9 @@
+// Simple Event Service (placeholder)
+class EventService {
+  async getAllEvents(userSettings) {
+    // Return empty array for now - this prevents errors
+    return []
+  }
+}
+
+export default new EventService()
